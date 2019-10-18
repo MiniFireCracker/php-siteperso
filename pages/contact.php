0c119190de8db0deeef330aaf9e6498dec7254bb
Gonzalez <lodi31@neuf.fr>
2019-10-18 16:30:56 +0200

    fgfg

--- pages/contact.php
+++ pages/contact.php
@@ -1,17 +1,17 @@
 
-<form action="save.php" method=GET>
+<form action="/save.php" method=GET>
     
     <div>
     <label>Nom:</label>    
-    <input type= text id=name name=name>
+    <input type= text id=name name=name required>
     </div>
     <div>
     <label>Mail:</label>    
-    <input type= email id=email name=email>
+    <input type= email id=email name=email required>
     </div>
     <div>
     <label>Mot de passe:</label>    
-    <input type= password id=password name=password>
+    <input type= password id=password name=password required >
     </div>  
     <button type=submit>Submit </button>
 
--- public/save.php
+++ public/save.php
@@ -1,12 +1,20 @@
 <?php 
 
 function saveUserData(){
-
-    $file= 'last_message.json';
     
-    file_put_contents($file, [htmlentities($_GET['name']), htmlentities($_GET['email']), htmlentities($_GET['password'])], FILE_APPEND );
+    $file= __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'last_message.json';
+   // $encoded_stuff = $_GET['name'];
+   
+    if ($_GET['name'] && $_GET['mail'] && $_GET['password']){
+  
+        $encoded_stuff = json_encode( htmlentities($_GET['name']), htmlentities($_GET['email']), htmlentities($_GET['password']));
+        file_put_contents($file, $encoded_stuff, FILE_APPEND );
 
-}
+    };
+    
+};
+
+saveUserData();
 
 
 ?>
